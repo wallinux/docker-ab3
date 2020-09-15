05b048fc1a5b3bcc897e02c3989c4a47fa82e069
Anders Wallin <anders.wallin@windriver.com>
2020-09-15 11:18:01 +0200

    lttng: updated
    Signed-off-by: Anders Wallin <anders.wallin@windriver.com>

--- lttng.mk
+++ lttng.mk
@@ -1,22 +1,41 @@
 # lttng.mk
+LTTNG_DISTRO		?= ubuntu-18.04
 LTTNG_TAGS		?= rcs stable-2.10 stable-2.11
-LTTNG_TAG		?= stable-2.11
+LTTNG_TAG		?= rcs
 LTTNG_IMAGE		= lttng:$(LTTNG_TAG)
 LTTNG_CONTAINER		= lttng_$(LTTNG_TAG)
 LTTNG_HOSTIP		= $(shell /sbin/ifconfig | grep 128.224 | cut -d: -f 2 | cut -d' ' -f 1)
 LTTNG_HOSTNAME          ?= lttng-$(subst .,_,$(LTTNG_TAG)).eprime.com
 ##LTTNG_PORTS		?= -p 5342:5342 -p 5343:5343 -p 5344:5344
-################################################################
 
+################################################################
 lttng.ALL: lttng.CREATE
 
+lttng.CREATE: # Create ALL lttng container
+	$(foreach tag,$(LTTNG_TAGS), make -s lttng.create LTTNG_TAG=$(tag); )
+
+lttng.RM: # Remove ALL lttng container
+	$(foreach tag, $(LTTNG_TAGS), make -s lttng.rm LTTNG_TAG=$(tag); )
+
+lttng.RMI: # Remove ALL lttng images
+	-$(foreach tag, $(LTTNG_TAGS), make -s lttng.rmi LTTNG_TAG=$(tag); )
+	-$(DOCKER) rmi lttng
+	$(call rmstamp,lttng.build)
+
+lttng.PUSH: # Remove ALL lttng container
+	$(foreach tag, $(LTTNG_TAGS), make -s lttng.push LTTNG_TAG=$(tag); )
+
+lttng.PULL: # Remove ALL lttng container
+	$(foreach tag, $(LTTNG_TAGS), make -s lttng.pull LTTNG_TAG=$(tag); )
+
+################################################################
 lttng.build: # Build lttng image
 	$(TRACE)
 	$(CP) $(HOME)/.gitconfig lttng/
-	$(DOCKER) build --pull -f lttng/Dockerfile -t "lttng" .
+	$(DOCKER) build --pull -f lttng/Dockerfile.$(LTTNG_DISTRO) -t "lttng" .
 	$(MKSTAMP)
 
-lttng.build.% : lttng.build # Build lttng.$(LTTNG_TAG) image
+lttng.build.% : lttng.build
 	$(TRACE)
 	$(DOCKER) build -f lttng/Dockerfile-lttng --build-arg LTTNG_TAG=$* -t "lttng:$*" .
 	$(MKSTAMP)
@@ -31,7 +50,7 @@ lttng.prepare:
 		sh -c "if [ ! -e /root/.ssh/id_rsa ]; then ssh-keygen -b 2048 -t rsa -f /root/.ssh/id_rsa -q -N ''; fi"
 	$(DOCKER) stop -t 2 $(LTTNG_CONTAINER)
 
-lttng.create.%: lttng.build.$(LTTNG_TAG) # Create a lttng container
+lttng.create.%: lttng.build.$(LTTNG_TAG)
 	$(TRACE)
 	$(DOCKER) create -P --name=$(LTTNG_CONTAINER) \
 		-h $(LTTNG_HOSTNAME) \
@@ -45,9 +64,6 @@ lttng.create.%: lttng.build.$(LTTNG_TAG) # Create a lttng container
 lttng.create: lttng.create.$(LTTNG_TAG) # Create lttng container
 	$(TRACE)
 
-lttng.CREATE: # Create ALL lttng container
-	$(foreach tag,$(LTTNG_TAGS), make -s lttng.create LTTNG_TAG=$(tag); )
-
 lttng.start: lttng.create # Start lttng container
 	$(TRACE)
 	$(DOCKER) start $(LTTNG_CONTAINER)
@@ -58,21 +74,13 @@ lttng.stop: # Stop lttng container
 
 lttng.rm: lttng.stop # Remove lttng container
 	$(TRACE)
-	$(DOCKER) rm $(LTTNG_CONTAINER) || true
+	-$(DOCKER) rm $(LTTNG_CONTAINER)
 	$(call rmstamp,lttng.create.$(LTTNG_TAG))
-
-lttng.RM: # Remove ALL lttng container
-	$(foreach tag, $(LTTNG_TAGS), make -s lttng.rm LTTNG_TAG=$(tag); )
 
 lttng.rmi: # Remove lttng image
 	$(TRACE)
-	$(DOCKER) rmi $(LTTNG_IMAGE)
+	-$(DOCKER) rmi $(LTTNG_IMAGE)
 	$(call rmstamp,lttng.build.$(LTTNG_TAG))
-
-lttng.RMI: # Remove ALL lttng images
-	-$(foreach tag, $(LTTNG_TAGS), make -s lttng.rmi LTTNG_TAG=$(tag); )
-	$(DOCKER) rmi lttng
-	$(call rmstamp,lttng.build)
 
 lttng.shell: lttng.start # Start a shell in lttng container
 	$(TRACE)
@@ -102,16 +110,8 @@ lttng.tag:
 lttng.push: lttng.tag # Push image to local registry
 	$(DOCKER) push $(REGISTRY_SERVER)/$(LTTNG_IMAGE)
 
-lttng.PUSH: # Remove ALL lttng container
-	$(foreach tag, $(LTTNG_TAGS), make -s lttng.push LTTNG_TAG=$(tag); )
-
 lttng.pull: # Pull image from local registry
 	$(DOCKER) pull $(REGISTRY_SERVER)/$(LTTNG_IMAGE)
-
-lttng.PULL: # Remove ALL lttng container
-	$(foreach tag, $(LTTNG_TAGS), make -s lttng.pull LTTNG_TAG=$(tag); )
-
-pull:: lttng.pull
 
 lttng.distclean: lttng.RM lttng.RMI
 
@@ -120,5 +120,9 @@ lttng.help:
 	$(call run-help, lttng.mk)
 	$(call run-note, "- Use the LTTNG_TAG(=$(LTTNG_TAG)) to define image/container")
 	$(call run-note, "- Supported LTTNGS_TAGS are: $(LTTNG_TAGS)")
+
+################################################################
+
+pull:: lttng.pull
 
 help:: lttng.help
